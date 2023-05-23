c62134386bf5221edbda6c44b2bb5f1cc165194c
AndreaDevelhope <rheienekenzz@gmail.com>
2023-05-23 23:03:20 +0200

    home screen completed(?)

--- app/src/main/java/co/develhope/meteoapp/ListAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/ListAdapter.kt
@@ -3,12 +3,12 @@ package co.develhope.meteoapp
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import co.develhope.meteoapp.databinding.HomePageItemBinding
+import co.develhope.meteoapp.databinding.HomeScreenItemBinding
 
 class ListAdapter(private val items:List<HomeScreenItemData>):RecyclerView.Adapter<ListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemBinding = HomePageItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        val itemBinding = HomeScreenItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return ViewHolder(itemBinding)
     }
 
@@ -18,14 +18,14 @@ class ListAdapter(private val items:List<HomeScreenItemData>):RecyclerView.Adapt
        holder.binding(items[position])
     }
 
-    inner class ViewHolder(private val binding: HomePageItemBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ViewHolder(private val binding: HomeScreenItemBinding) : RecyclerView.ViewHolder(binding.root){
        fun binding (item:HomeScreenItemData){
-           binding.day.setText(item.day)
+           binding.currentDay.setText(item.day)
            binding.wcIcon.setImageResource(item.weather)
-           binding.wind.text = item.wind.toString()
-           binding.tempMin.text = item.temp_min.toString()
-           binding.tempMax.text = item.temp_max.toString()
-           binding.mm.text = item.mm_rain.toString()
+           binding.windValue.text = item.wind.toString()
+           binding.minTemperatureValue.text = item.temp_min.toString()
+           binding.maxTemperatureValue.text = item.temp_max.toString()
+           binding.rainfallValue.text = item.mm_rain.toString()
        }
     }
 }
