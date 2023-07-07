f168fa9c5473f0d4c653d4f97c4ee74463052e79
Margheimpro <126887580+margheimpro@users.noreply.github.com>
2023-07-07 22:27:53 +0200

    added date management in today and tomorrow, APIs and Screens

--- app/src/main/java/co/develhope/meteoapp/features/today/TodayScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/features/today/TodayScreen.kt
@@ -1,15 +1,21 @@
 package co.develhope.meteoapp.features.today
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.develhope.meteoapp.databinding.ScreenTodayBinding
 import co.develhope.meteoapp.features.data.ForecastResult
-
+import co.develhope.meteoapp.features.network.DateUtils
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Locale
 
 
 class TodayScreen : Fragment() {
@@ -17,6 +23,9 @@ class TodayScreen : Fragment() {
     private lateinit var binding: ScreenTodayBinding
 
     private val viewModel: TodayViewModel by viewModels()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val currentDate = DateUtils.getDateForTodayAndTomorrowScreen("${LocalDate.now()}")
 
 
     override fun onCreateView(
@@ -29,6 +38,7 @@ class TodayScreen : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -37,6 +47,10 @@ class TodayScreen : Fragment() {
         viewModel.forecastLiveData.observe(viewLifecycleOwner){
             showTodayMeteo(it)
         }
+
+        binding.todayTitleDate.text = currentDate
+
+
     }
 
     private fun showTodayMeteo(meteo: ForecastResult){
--- app/src/main/java/co/develhope/meteoapp/features/tomorrow/TomorrowScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/features/tomorrow/TomorrowScreen.kt
@@ -1,24 +1,32 @@
 package co.develhope.meteoapp.features.tomorrow
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.develhope.meteoapp.R
 import co.develhope.meteoapp.databinding.ScreenTomorrowBinding
 import co.develhope.meteoapp.features.data.ForecastResult
+import co.develhope.meteoapp.features.network.DateUtils
 import co.develhope.meteoapp.features.today.TodayTomorrowAdapter
 import co.develhope.meteoapp.features.today.TodayTomorrowWeatherCondition
 import co.develhope.meteoapp.features.today.TodayViewModel
+import java.time.LocalDate
 
 class TomorrowScreen : Fragment() {
 
     private lateinit var binding: ScreenTomorrowBinding
 
     private val viewModel: TomorrowViewModel by viewModels()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val tomorrowDate = DateUtils.getDateForTodayAndTomorrowScreen("${LocalDate.now().plusDays(1)}")
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +37,7 @@ class TomorrowScreen : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -37,6 +46,8 @@ class TomorrowScreen : Fragment() {
         viewModel.forecastLiveData.observe(viewLifecycleOwner){
             showTomorrowMeteo(it)
         }
+
+        binding.tomorrowTitleDate.text = tomorrowDate
     }
 
     private fun showTomorrowMeteo(meteo: ForecastResult){
