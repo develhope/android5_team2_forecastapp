e32f3bea1cfea24baa39ac3a5ed1edb87fdb7f45
Margheimpro <126887580+margheimpro@users.noreply.github.com>
2023-07-07 15:52:40 +0200

    added wind direction

--- app/src/main/java/co/develhope/meteoapp/features/data/Hourly2.kt
+++ app/src/main/java/co/develhope/meteoapp/features/data/Hourly2.kt
@@ -13,6 +13,7 @@ data class Hourly2(
     val cloudcover: List<Int>,
     @SerializedName("windspeed_10m") val windspeed10m: List<Double>,
     @SerializedName("uv_index") val uvIndex: List<Double>,
-    @SerializedName("is_day") val isDay: List<Int>
-    )
+    @SerializedName("is_day") val isDay: List<Int>,
+    @SerializedName("winddirection_10m") val windDirection: List<Int>
+)
 
