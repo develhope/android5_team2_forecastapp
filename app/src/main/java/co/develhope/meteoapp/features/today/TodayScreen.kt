7eb6dde9a558879c0182031717aca5fc761b9159
AndreaDevelhope <rheienekenzz@gmail.com>
2023-07-10 18:35:15 +0200

    added a behaviour in the today screen so that the weatherforecast is directly shown starting from the current hour, shrinked temperature values in the home screen to show just the int

--- app/src/main/java/co/develhope/meteoapp/features/today/TodayScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/features/today/TodayScreen.kt
@@ -2,7 +2,6 @@ package co.develhope.meteoapp.features.today
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
