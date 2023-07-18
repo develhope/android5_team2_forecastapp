1aae39dbc981c3f9999c06cb4bd6236ed018b6ae
Margheimpro <126887580+margheimpro@users.noreply.github.com>
2023-07-18 16:02:09 +0200

    little modifies in layouts today and tomorrow

--- app/src/main/java/co/develhope/meteoapp/features/MyApplication.kt
+++ app/src/main/java/co/develhope/meteoapp/features/MyApplication.kt
@@ -8,16 +8,11 @@ import co.develhope.meteoapp.features.ui.viewModels
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-// Serve per utilizzare le shared preferences
 class MyApplication:Application() {
-    //lateinit var preferences: SharedPreferences
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
-/*        preferences = getSharedPreferences("app", Context.MODE_PRIVATE)
-        preferences.edit().putFloat("latitude",37.49F).apply()
-        preferences.edit().putFloat("longitude",15.07F).apply()*/
 
         startKoin{
             androidContext(this@MyApplication)
