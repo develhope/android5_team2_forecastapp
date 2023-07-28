3264dbafc847177601da4de75ef04bf7e0c1762e
AndreaDevelhope <rheienekenzz@gmail.com>
2023-07-28 14:23:24 +0200

    Added GeoLocalizationHelper.kt and added the behaviour that when the app is opened for the first time and the position is not active, it will redirect you to the search screen and force you to search a city before continuing. Added the behaviour that when you click on a search item it navigates you to the home screen

--- app/src/main/java/co/develhope/meteoapp/features/ui/todaytomorrow/tomorrow/TomorrowScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/features/ui/todaytomorrow/tomorrow/TomorrowScreen.kt
@@ -1,24 +1,21 @@
 package co.develhope.meteoapp.features.ui.todaytomorrow.tomorrow
 
-import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.develhope.meteoapp.R
 import co.develhope.meteoapp.databinding.ScreenTomorrowBinding
+import co.develhope.meteoapp.features.data.local.DateUtils
 import co.develhope.meteoapp.features.data.local.SharedPreferencesHelper
 import co.develhope.meteoapp.features.data.remote.models.WeatherConditions
-import co.develhope.meteoapp.features.data.local.DateUtils
 import co.develhope.meteoapp.features.ui.todaytomorrow.TodayTomorrowAdapter
-import co.develhope.meteoapp.features.ui.todaytomorrow.today.TodayViewModel
 import org.koin.android.ext.android.inject
 import java.time.LocalDate
 
